69a194416dc2b241aab75cd8be10cbbfdb565e8b
Verga <verga.wiktor@galileo.galileicrema.it>
2022-07-18 12:20:25 +0200

    Modificata Pagina Libri

---
+++ Javascript/Libri.js
@@ -0,0 +1,9 @@
+function listaIta() {
+    document.getElementById('ITALIB2').style.display = 'flex';
+    document.getElementById('ITALIB').style.display = 'none';
+}
+
+function listaMate() {
+    document.getElementById('MATELIB2').style.display = 'flex';
+    document.getElementById('MATELIB').style.display = 'none';
+}
